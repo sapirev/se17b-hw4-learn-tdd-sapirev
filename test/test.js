3318d376b49c5043472695e8662bf5623c41ce26
sapirev <sapir.eliyahu12@gmail.com>
2017-05-31 20:08:44 +0300

    GREEN

--- test/test.js
+++ test/test.js
@@ -1,13 +1,7 @@
 var assert = require("assert"); // core module
 var C = require('../cash.js');  // our module
 
-C.getChange = function (totalPayable, cashPaid) {
-    'use strict';
-    return [50, 20, 20];    // just enough to pass :-)
-}
-
-describe('Cash Register', function(){
-  describe('Module C', function(){
+describe('Module C', function(){
    it('getChange(210,300) should equal [50,20,20]', function(){
     assert.deepEqual(C.getChange(210,300), [50,20,20]);
      })
@@ -19,4 +13,4 @@ describe('Cash Register', function(){
       assert.equal(typeof C.getChange, 'function');
     })
   })
-});
+;
