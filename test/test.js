46df2187b905dc6350f4667be77f436e564bb546
sapirev <sapir.eliyahu12@gmail.com>
2017-05-31 19:53:03 +0300

    RED

--- test/test.js
+++ test/test.js
@@ -10,7 +10,10 @@ describe('Cash Register', function(){
   describe('Module C', function(){
    it('getChange(210,300) should equal [50,20,20]', function(){
     assert.deepEqual(C.getChange(210,300), [50,20,20]);
-     }) 
+     })
+   it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){
+    assert.deepEqual(C.getChange(486,1000), [500, 10, 2, 2]);
+     })
    it('should have a getChange Method', function(){
       assert.equal(typeof C, 'object');
       assert.equal(typeof C.getChange, 'function');
